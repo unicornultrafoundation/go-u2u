6f8760b198f10cd8aec94ac97551548a00000c3f
phatngluu <yadsendew@protonmail.com>
2023-08-15 16:35:49 +0700

    upgrade sfc contract

---
+++ u2u/contracts/sfclib/sfclib_predeploy.go
@@ -0,0 +1,17 @@
+package sfclib
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/unicornultrafoundation/go-u2u/gossip/contract/sfclib100"
+)
+
+// GetContractBin is SFCLib contract genesis implementation bin code
+// Has to be compiled with flag bin-runtime
+// Built from opera-sfc 424031c81a77196f4e9d60c7d876032dd47208ce, solc 0.5.17+commit.d19bba13.Emscripten.clang, optimize-runs 200
+func GetContractBin() []byte {
+	return hexutil.MustDecode(sfclib100.ContractBinRuntime)
+}
+
+// ContractAddress is the SFCLib contract address
+var ContractAddress = common.HexToAddress("0xfc01face00000000000000000000000000000000")

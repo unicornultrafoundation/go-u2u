77296ccde183f0d3828fb8aca6a160977e577299
ThuongThaiHuu <huuthuong@uniultra.xyz>
2023-08-17 10:32:58 +0700

    [Feat] Configuration network properties
    Signed-off-by: ThuongThaiHuu <huuthuong@uniultra.xyz>

---
+++ cmd/u2u/launcher/gentxs/config.go
@@ -0,0 +1,57 @@
+package gentxs
+
+import (
+	"errors"
+
+	"github.com/unicornultrafoundation/go-u2u/u2u"
+	"github.com/ethereum/go-ethereum/common"
+	"gopkg.in/urfave/cli.v1"
+
+	"github.com/unicornultrafoundation/go-u2u/utils/toml"
+)
+
+var ConfigFileFlag = cli.StringFlag{
+	Name:  "config",
+	Usage: "TOML configuration file",
+	Value: "txsgen.toml",
+}
+
+type Config struct {
+	ChainId int64 // chain id for sign transactions
+	Payer   common.Address
+	URLs    []string // WS nodes API URL
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		ChainId: int64(u2u.FakeNetworkID),
+		URLs: []string{
+			"ws://127.0.0.1:4500",
+		},
+	}
+}
+
+func OpenConfig(ctx *cli.Context) *Config {
+	cfg := DefaultConfig()
+	f := ctx.GlobalString(ConfigFileFlag.Name)
+	err := cfg.Load(f)
+	if err != nil {
+		panic(err)
+	}
+	return cfg
+}
+
+func (cfg *Config) Load(file string) error {
+	data, err := toml.ParseFile(file)
+	if err != nil {
+		return err
+	}
+
+	err = toml.Settings.UnmarshalTable(data, cfg)
+	if err != nil {
+		err = errors.New(file + ", " + err.Error())
+		return err
+	}
+
+	return nil
+}

48c8ce1af781d65436e344d66e578ca898a84a81
lewtran <lewtan@outlook.com>
2024-01-11 15:11:08 +0700

    update tx tracing testunit

--- cmd/u2u/launcher/txtracer_test.go
+++ cmd/u2u/launcher/txtracer_test.go
@@ -12,7 +12,6 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-
 	"github.com/unicornultrafoundation/go-u2u/evmcore/txtracer"
 )
 
@@ -23,19 +22,44 @@ func TestTxTracing(t *testing.T) {
 	wsport := strconv.Itoa(trulyRandInt(10000, 65536))
 	cliNode := exec(t,
 		"--fakenet", "1/1", "--enabletxtracer", "--port", "0", "--maxpeers", "0", "--nodiscover", "--nat", "none",
-		"--ws", "--ws.port", wsport, "--http", "--http.api", "eth,web3,net,txpool,trace", "--http.port", port, "--allow-insecure-unlock",
-		"--cache", "7923", "--datadir.minfreedisk", "1")
+		"--ws", "--ws.port", wsport, "--http", "--http.api", "eth,web3,net,txpool,trace", "--http.port", port, "--allow-insecure-unlock")
 
 	// Wait for node to start
 	endpoint := "ws://127.0.0.1:" + wsport
 	waitForEndpoint(t, endpoint, 60*time.Second)
 
 	// Deploy a smart contract from the testdata javascript file
-	cliConsoleDeploy := exec(t, "attach", "--datadir", cliNode.Datadir, "--exec", "loadScript('testdata/txtracer_test.js')")
-	cliConsoleOutput := strings.Split(string(*cliConsoleDeploy.GetOutPipeData()), "\n")
-	txHashCall := cliConsoleOutput[1]
-	txHashDeploy := cliConsoleOutput[2]
+	cliConsoleDeploy := exec(t, "attach", "--datadir", cliNode.Datadir, "--exec", "loadScript('testdata/txTracingTest.js')")
+	contractAddress := string(*cliConsoleDeploy.GetOutPipeData())
+	contractAddress = contractAddress[strings.Index(contractAddress, "0x") : len(contractAddress)-1]
 	cliConsoleDeploy.WaitExit()
+
+	abi := `[{"inputs":[],"name":"deploy","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getA","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_a","type":"uint256"}],"name":"setA","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_a","type":"uint256"}],"name":"setInA","outputs":[],"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"tst","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]`
+
+	cliConsole := exec(t, "attach", "--datadir", cliNode.Datadir)
+	cliConsoleOutput := *cliConsole.GetOutDataTillCursor()
+
+	// Initialize test contract for interaction
+	cliConsole.InputLine("var abi=" + abi)
+	cliConsoleOutput = *cliConsole.GetOutDataTillCursor()
+	cliConsole.InputLine(`var testContract = u2u.contract(abi)`)
+	cliConsoleOutput = *cliConsole.GetOutDataTillCursor()
+	cliConsole.InputLine("testContract = testContract.at('" + contractAddress + "')")
+	cliConsoleOutput = *cliConsole.GetOutDataTillCursor()
+
+	// Call simple contract call to check created trace
+	cliConsole.InputLine("testContract.setA.sendTransaction(24, {from:u2u.accounts[1]})")
+	cliConsoleOutput = *cliConsole.GetOutDataTillCursor()
+	txHashCall := cliConsoleOutput[strings.Index(cliConsoleOutput, "0x") : len(cliConsoleOutput)-3]
+
+	cliConsole.InputLine("testContract.deploy.sendTransaction({from:u2u.accounts[1]})")
+	cliConsoleOutput = *cliConsole.GetOutDataTillCursor()
+	txHashDeploy := cliConsoleOutput[strings.Index(cliConsoleOutput, "0x") : len(cliConsoleOutput)-3]
+	time.Sleep(5000 * time.Millisecond)
+
+	// Close node console
+	cliConsole.InputLine("exit")
+	cliConsole.WaitExit()
 
 	traceResult1, err := getTrace(txHashCall, port)
 	if err != nil {
@@ -72,7 +96,6 @@ func TestTxTracing(t *testing.T) {
 }
 
 func getTrace(txHash string, nodePort string) (response, error) {
-
 	jsonStr := []byte(`{"method":"trace_transaction","params":["` + txHash + `"],"id":1,"jsonrpc":"2.0"}`)
 	resp, err := http.Post("http://127.0.0.1:"+nodePort, "application/json", bytes.NewBuffer(jsonStr))
 	if err != nil {

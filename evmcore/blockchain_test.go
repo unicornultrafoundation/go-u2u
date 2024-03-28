9043a9fc447af8a6192c7824090b9e83e33cb866
b1m0n <bebibemon@gmail.com>
2024-03-28 18:19:58 +0700

    [WIP] Unit tests

--- evmcore/blockchain_test.go
+++ evmcore/blockchain_test.go
@@ -1,6 +1,7 @@
 package evmcore
 
 import (
+	"github.com/unicornultrafoundation/go-u2u/core/state"
 	"math/big"
 	"testing"
 
@@ -8,73 +9,46 @@ import (
 	"github.com/unicornultrafoundation/go-u2u/core"
 	"github.com/unicornultrafoundation/go-u2u/core/rawdb"
 	"github.com/unicornultrafoundation/go-u2u/core/types"
-	"github.com/unicornultrafoundation/go-u2u/core/vm"
 	"github.com/unicornultrafoundation/go-u2u/crypto"
 	"github.com/unicornultrafoundation/go-u2u/params"
 )
 
 func TestEIP712Transition(t *testing.T) {
+	chainCfg := params.TestChainConfig
+	chainCfg.EIP712Block = big.NewInt(1)
 	var (
-		aa = common.HexToAddress("0x000000000000000000000000000000000000aaaa")
 		// A sender who makes transactions, has some funds
 		key, _  = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
 		address = crypto.PubkeyToAddress(key.PublicKey)
 		funds   = big.NewInt(1000000000000000)
-		gspec   = &core.Genesis{
-			Config: params.TestChainConfig,
-			Alloc: core.GenesisAlloc{
-				address: {Balance: funds},
-				// The address 0xAAAA sloads 0x00 and 0x01
-				aa: {
-					Code: []byte{
-						byte(vm.PC),
-						byte(vm.PC),
-						byte(vm.SLOAD),
-						byte(vm.SLOAD),
-					},
-					Nonce:   0,
-					Balance: big.NewInt(0),
-				},
-			},
+		gspec   = &core.Genesis{Config: chainCfg}
+		gen     = func(i int, b *BlockGen) {
+			b.SetCoinbase(common.Address{1})
+			pmParams := &types.PaymasterParams{
+				Paymaster:      nil,
+				PaymasterInput: nil,
+			}
+			if i == 2 {
+				pmParams = nil
+			}
+			// Construct 1 normal EIP-712 tx for each block
+			signer := types.LatestSigner(gspec.Config)
+			tx, _ := types.SignNewTx(key, signer, &types.EIP712Tx{
+				ChainID:         gspec.Config.ChainID,
+				Nonce:           0,
+				To:              &common.Address{},
+				Gas:             30000,
+				GasPrice:        b.header.BaseFee,
+				PaymasterParams: pmParams,
+			})
+			b.AddTx(tx)
 		}
-	)
-	// Generate blocks
-	_, blocks, _ := GenerateChainWithGenesis(gspec, engine, 1, func(i int, b *BlockGen) {
-		b.SetCoinbase(common.Address{1})
-
-		// One transaction to 0xAAAA
-		signer := types.LatestSigner(gspec.Config)
-		tx, _ := types.SignNewTx(key, signer, &types.AccessListTx{
-			ChainID:  gspec.Config.ChainID,
-			Nonce:    0,
-			To:       &aa,
-			Gas:      30000,
-			GasPrice: b.header.BaseFee,
-			AccessList: types.AccessList{{
-				Address:     aa,
-				StorageKeys: []common.Hash{{0}},
-			}},
+		db         = rawdb.NewMemoryDatabase()
+		statedb, _ = state.New(common.Hash{}, state.NewDatabase(db), nil)
+		genesis    = MustApplyFakeGenesis(statedb, FakeGenesisTime, map[common.Address]*big.Int{
+			address: funds,
 		})
-		b.AddTx(tx)
-	})
+	)
+	blocks, _, chain := GenerateChain(chainCfg, genesis, db, 3, gen)
 
-	// Import the canonical chain
-	chain, err := NewBlockChain(rawdb.NewMemoryDatabase(), gspec, nil, engine, vm.Config{}, nil, nil)
-	if err != nil {
-		t.Fatalf("failed to create tester chain: %v", err)
-	}
-	defer chain.Stop()
-
-	if n, err := chain.InsertChain(blocks); err != nil {
-		t.Fatalf("block %d: failed to insert into chain: %v", n, err)
-	}
-
-	block := chain.GetBlockByNumber(1)
-
-	// Expected gas is intrinsic + 2 * pc + hot load + cold load, since only one load is in the access list
-	expected := params.TxGas + params.TxAccessListAddressGas + params.TxAccessListStorageKeyGas +
-		vm.GasQuickStep*2 + params.WarmStorageReadCostEIP2929 + params.ColdSloadCostEIP2929
-	if block.GasUsed() != expected {
-		t.Fatalf("incorrect amount of gas spent: expected %d, got %d", expected, block.GasUsed())
-	}
 }

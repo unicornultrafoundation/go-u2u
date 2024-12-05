5628092a384f1b02609fcfae4ff8604385912e3e
huongnguyenduc <duchuong007@gmail.com>
2024-12-05 11:52:20 +0700

    Fix bootnodes flag unable to read

---
+++ params/bootnodes.go
@@ -0,0 +1,7 @@
+package params
+
+// MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
+// the main Ethereum network.
+var MainnetBootnodes = []string{}
+
+var V5Bootnodes = []string{}

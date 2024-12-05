b8a50248d539af75986884ab9c00f35f3131cb88
trinhdn97 <trinhdn97.work@gmail.com>
2024-12-05 18:11:24 +0700

    Add default mainnet bootnodes and bootnodesv5

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -1,7 +1,15 @@
 package params
 
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
-// the main Ethereum network.
-var MainnetBootnodes = []string{}
+// the main U2U network.
+var MainnetBootnodes = []string{
+	"enode://21dfee41ddd127ebbd68fb14b39945f6e993ad9eb35c57e5e2e17ec1740960400d6d174f6c119fb9940072eec2d468ee5d767752bf9a44900ac8ac6d6de61330@18.143.208.170:5050", // bootnode-1
+	"enode://a1e1999ab32c7ea71b3fb4fd4e2143beadc3f71365e2a5a0e54e15780d28e5a80576a387406d9b60eee7c31289618c6a5ef93bfe295215518cecbf23bc50211e@3.1.11.147:5050",     // bootnode-2
+	"enode://89f9063d0ce1eb0780a82f258cc89acae3ad14e744680a7fb5ea8be468835ad388fdce26bb8ee781b11b0f2dcf5bc9f88964a18a5cd138e9ab3d6bbb0489a254@13.215.34.27:5050",   // bootnode-3
+}
 
-var V5Bootnodes = []string{}
+var V5Bootnodes = []string{
+	"enr:-J24QHysbJGPA_BL2-ZJ2un6yASWpSv5HWkXXndRM-gm-X6zGIF4DEYYSdxxAG-QwWi1emB_YlgD11wihDs5M3RkwDMFgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQIh3-5B3dEn671o-xSzmUX26ZOtnrNcV-Xi4X7BdAlgQIRzbmFwwIN0Y3CCE7qDdTJ1x8aE0bui44CDdWRwghO6", // bootnode-1
+	"enr:-J24QGnMbFIW5e_AQV5Rh0DXP-YahvvLALy7-cXMC5p2QrD3W9SYGNgLf3f4dWYfB4YU9j4qE5L1w0aeODmR5NgO9QMHgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQKh4Zmasyx-pxs_tP1OIUO-rcP3E2XipaDlThV4DSjlqIRzbmFwwIN0Y3CCE7qDdTJ1x8aE0bui44CDdWRwghO6", // bootnode-2
+	"enr:-J24QCkBwE_32Dot66xbHlJlw9pQwtqN4GOlz0DM7tFcqC3vArNRSK8kmvgZRwfAnQcEuUY_to6bZmJXUuxKvwsu49oEgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQKJ-QY9DOHrB4CoLyWMyJrK460U50RoCn-16ovkaINa04RzbmFwwIN0Y3CCE7qDdTJ1x8aE0bui44CDdWRwghO6", // bootnode-3
+}

ae3471c13fdc77494f96fc85334b6adc97f3bc12
trinhdn97 <trinhdn97.work@gmail.com>
2025-07-17 16:33:32 +0700

    Init caution package for handling errors

--- cmd/u2u/launcher/snapshotcmd.go
+++ cmd/u2u/launcher/snapshotcmd.go
@@ -67,7 +67,7 @@ var (
 					utils.BloomFilterSizeFlag,
 				},
 				Description: `
-geth snapshot prune-state <state-root>
+u2u snapshot prune-state <state-root>
 will prune historical state data with the help of the state snapshot.
 All trie nodes and contract codes that do not belong to the specified
 version state will be deleted from the database. After pruning, only
@@ -77,7 +77,7 @@ The default pruning target is the HEAD state.
 
 WARNING: It's necessary to delete the trie clean cache after the pruning.
 If you specify another directory for the trie clean cache via "--cache.trie.journal"
-during the use of Geth, please also specify it here for correct deletion. Otherwise
+during the use of u2u, please also specify it here for correct deletion. Otherwise
 the trie clean cache with default directory will be deleted.
 `,
 			},
@@ -92,7 +92,7 @@ the trie clean cache with default directory will be deleted.
 					utils.AncientFlag,
 				},
 				Description: `
-geth snapshot verify-state <state-root>
+u2u snapshot verify-state <state-root>
 will traverse the whole accounts and storages set based on the specified
 snapshot and recalculate the root hash of state for verification.
 In other words, this command does the snapshot to trie conversion.
@@ -109,7 +109,7 @@ In other words, this command does the snapshot to trie conversion.
 					utils.AncientFlag,
 				},
 				Description: `
-geth snapshot traverse-state <state-root>
+u2u snapshot traverse-state <state-root>
 will traverse the whole state from the given state root and will abort if any
 referenced trie node or contract code is missing. This command can be used for
 state integrity verification. The default checking target is the HEAD state.
@@ -128,7 +128,7 @@ It's also usable without snapshot enabled.
 					utils.AncientFlag,
 				},
 				Description: `
-geth snapshot traverse-rawstate <state-root>
+u2u snapshot traverse-rawstate <state-root>
 will traverse the whole state from the given root and will abort if any referenced
 trie node or contract code is missing. This command can be used for state integrity
 verification. The default checking target is the HEAD state. It's basically identical

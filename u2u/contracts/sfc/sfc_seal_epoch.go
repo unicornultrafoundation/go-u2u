186b5f99da0c0a1b3f986d9eb35de2cea6381842
Khang Nguyen Truong <72505073+keohanoi@users.noreply.github.com>
2025-09-16 16:19:17 +0700

    Refactor fee share transfer logic in handleInternalSealEpochRewards to use Transfer helper and add balance check

--- u2u/contracts/sfc/sfc_seal_epoch.go
+++ u2u/contracts/sfc/sfc_seal_epoch.go
@@ -813,37 +813,13 @@ func handleInternalSealEpochRewards(evm *vm.EVM, epochDuration *big.Int, current
 
 		// Simply transfer the tokens to the treasury address
 		log.Info("handleInternalSealEpochRewards: transferring fee share to treasury", "treasuryAddr", treasuryAddr, "feeShare", feeShare.String())
-		evm.SfcStateDB.AddBalance(treasuryAddr, feeShare)
-		evm.SfcStateDB.SubBalance(ContractAddress, feeShare)
+		if !evm.Context.CanTransfer(evm.SfcStateDB, ContractAddress, feeShare) {
+			log.Error("handleInternalSealEpochRewards: insufficient balance for fee share transfer", "treasuryAddr", treasuryAddr, "feeShare", feeShare.String())
+			return gasUsed, vm.ErrExecutionReverted
+		}
+		// Use the Transfer helper function to transfer the tokens
+		evm.Context.Transfer(evm.SfcStateDB, ContractAddress, treasuryAddr, feeShare)
 		
-		// oldBalance := evm.SfcStateDB.GetBalance(treasuryAddr);
-		// log.Debug("handleInternalSealEpochRewards: transferring fee share to treasury",
-		// 	"treasuryAddr", treasuryAddr,
-		// 	"feeShare", common.Bytes2Hex(feeShare.Bytes()),
-		// 	"oldBalance", oldBalance.String(),
-		// )
-		// // Then make a call to transfer the tokens to the treasury address
-		// // This simulates the Solidity code: treasuryAddress.CallSFC.value(feeShare)("");
-		// callData := []byte{} // Empty call data
-		// _, _, err = evm.CallSFC(
-		// 	vm.AccountRef(ContractAddress), // Caller
-		// 	treasuryAddr,                   // Target address
-		// 	callData,                       // Call data (empty)
-		// 	21000,                          // Gas limit for a simple transfer
-		// 	feeShare,                       // Value to transfer
-		// )
-		// log.Debug("Fee share transfered", "amount", feeShare.String())
-		// // expected treasury address balance to increase
-		// newBalance := evm.SfcStateDB.GetBalance(treasuryAddr);
-		// log.Debug("handleInternalSealEpochRewards: transferred fee share to treasury",
-		// 	"treasuryAddr", treasuryAddr,
-		// 	"feeShare", common.Bytes2Hex(feeShare.Bytes()),
-		// 	"newBalance", newBalance.String(),
-		// 	"balanceDiff", newBalance.Sub(newBalance, oldBalance).String(),
-		// )
-		// if err != nil {
-		// 	return gasUsed, err
-		// }
 		gasUsed += 21000 // Add gas for the transfer
 	}
 
